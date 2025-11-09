b8c66b0a807f1030b7561afd9cb12d7828fdb8f2
Aria Aura Rachman <ariaaurar@gmail.com>
2025-11-09 22:24:36 +0700

    Increase maximum count limit from 20 to 25

--- user/userAuth/service.js
+++ user/userAuth/service.js
@@ -32,7 +32,7 @@ const jwt = require('jsonwebtoken');
     }
 
     const result = await Count(topik);
-    const MAX = 20
+    const MAX = 25
     
     if(result >= MAX){
         throw new Error(`Pendaftaran untuk topik ${topik} sudah penuh`);
